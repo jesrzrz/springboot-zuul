49d88779707db885ffbe8b798c56c5233e5e98af
jrodriguez <jrodriguez@Desarrollo5.msolution.local>
2018-06-27 12:41:56 +0200

    initial commit

---
+++ src/main/java/es/jr/sbzu/ServiceDiscoveryApp.java
@@ -0,0 +1,13 @@
+package es.jr.sbzu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceDiscoveryApp {
+	 public static void main(String[] args) {		 
+	        SpringApplication.run(ServiceDiscoveryApp.class, args);
+    }
+}
